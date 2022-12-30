1a2b65a49a8765a7758fb58b203ceb6c3b4d0f37
Muhammad Ali Minhas <113331851+MuhammadAliMinhas@users.noreply.github.com>
2022-12-30 18:44:36 +0500

    Add files via upload
    Prolog Activities and Tasks

---
+++ Lab_13_Activity_1.pl
@@ -0,0 +1,9 @@
+female(rehana).
+female(sadia).
+female(aneela).
+
+male(sohaib).
+male(john).
+male(mohsin).
+male(qudrat).
+
---
+++ Lab_13_Activity_2.pl
@@ -0,0 +1,5 @@
+listensToMusic(mia).
+happy(yolanda).
+playAirGuitar(mia) :- listensToMusic(mia),
+    write('Mia plays air guitar if she listens to music').
+
