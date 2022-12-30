b04010ed89b86e1522c92808eb9a4709b79ba106
Muhammad Ali Minhas <113331851+MuhammadAliMinhas@users.noreply.github.com>
2022-12-30 18:41:00 +0500

    Add files via upload
    Prolog Activities and Tasks

---
+++ Prolog_Activity_03.pl
@@ -0,0 +1,6 @@
+sing_a_song(ananya).
+listens_to_music(rohit).
+listens_to_music(ananya) :- sing_a_song(ananya).
+happy(ananya) :- sing_a_song(ananya).
+happy(rohit) :- listens_to_music(rohit).
+playes_guitar(rohit) :- listens_to_music(rohit).
